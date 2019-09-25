c401745e4ef76f30c06c57ef6cba10c95250cc20
TamirSlo <tamir.uk.slo@gmail.com>
2019-09-25 16:09:02 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-README.md
+# Simple Pokemon UI website
 
+All contents of the website is within public_html.
 
-test
+Configuration is written in public_html/assets/conf.php for Database Connection etc.
+
+By Tamir Slobodskoy.
---
+++ public_html/index.php
@@ -0,0 +1,17 @@
+<?php
+
+include("assets/main.php");
+
+?>
+
+<html>
+    <head>
+        <title>Simple Pokemon Site</title>
+    </head>
+
+    <body>
+        <nav id="top">
+        Home | Browse | Compare | Login
+        </nav>
+    </body>
+</html>
